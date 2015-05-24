464f23388668444f5b3e9d99424f04efeaa610ee
Vitaliy <GarazdVZ@gmail.com>
2015-05-24 21:14:41 +0300

    Add BotinkiProduct

---
+++ src/BotinkiProduct.java
@@ -0,0 +1,22 @@
+public class BotinkiProduct extends Product
+{
+    private int size;
+    private String color;
+
+    public int getSize()
+    {
+        return size;
+    }
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+}
